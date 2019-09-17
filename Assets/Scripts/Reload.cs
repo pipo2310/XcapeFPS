d21d89a58dfced676d12231090e51fc247ac7616
Jose Quiros Montero <a85036@ecci.ucr.ac.cr>
2019-09-16 20:54:23 -0600

    añade archivos del proyecto

---
+++ Assets/Scripts/Reload.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reload : MonoBehaviour
+{
+    public AudioSource ReloadSound;
+    public GameObject CrossObject;
+    public GameObject MechanicsObject;
+    public int ClipCount;
+    public int ReserveCount;
+    public int ReloadAvailable;
+    public DisparoAccion GunComponent;
+
+    void Start()
+    {
+        GunComponent = GetComponent<DisparoAccion>();
+    }
+
+    void Update()
+    {
+        ClipCount = AmmoGlobal.LoadedAmmo;
+        ReserveCount = AmmoGlobal.CurrentAmmo;
+
+        if (ReserveCount == 0)
+        {
+            ReloadAvailable = 0;
+        }
+        else
+        {
+            ReloadAvailable = 10 - ClipCount;
+        }
+
+        if (Input.GetButtonDown("Reload"))
+        {
+            if (ReloadAvailable >= 1)
+            {
+                if (ReserveCount <= ReloadAvailable)
+                {
+                    AmmoGlobal.LoadedAmmo += ReserveCount;
+                    AmmoGlobal.CurrentAmmo -= ReserveCount;
+                    ActionReload();
+                }
+                else
+                {
+                    AmmoGlobal.LoadedAmmo += ReloadAvailable;
+                    AmmoGlobal.CurrentAmmo -= ReloadAvailable;
+                    ActionReload();
+                }
+            }
+            StartCoroutine(EnableScripts());
+
+        }
+    }
+
+    IEnumerator EnableScripts()
+    {
+        yield return new WaitForSeconds(1.1f);
+        GunComponent.enabled = true;
+        CrossObject.SetActive(true);
+        MechanicsObject.SetActive(true);
+    }
+
+    void ActionReload()
+    {
+        GunComponent.enabled = false;
+        CrossObject.SetActive(false);
+        MechanicsObject.SetActive(false);
+        ReloadSound.Play();
+        GetComponent<Animation>().Play("Reload");
+    }
+}
