6c777242cb6b786a4643a47d698c5d28a07208e2
José Fernando Quirós Montero <jfqm26@gmail.com>
2019-11-06 14:23:42 -0600

    añade menús de la rama Menu

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+	
+	public void PlayLevel1(){
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+	
+	public void PlayLevel2(){
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+	
+	public void PlayLevel3(){
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+		
+	public void QuitGame(){
+	   Debug.Log("QUIT");
+	   Application.Quit();
+	}
+}
