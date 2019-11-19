47bdda0334551165b5db3e1f098c978590ce4d88
José Fernando Quirós Montero <jfqm26@gmail.com>
2019-11-19 11:30:25 -0600

    arregla monitor de juego

--- Assets/Scripts/HealthMonitor.cs
+++ Assets/Scripts/HealthMonitor.cs
@@ -18,66 +18,100 @@ public class HealthMonitor : MonoBehaviour
     {
        // CriticalLife.SetActive(false);
         CurrentHealth = Vida.vidaJugador;
-        if (CurrentHealth==80) {
-            if (Vida100.transform.localScale.y <= 0.0f)
-            {
-                Vida100.SetActive(false);
-            }
-            else
-            {
-                Vida100.transform.localScale = new Vector3(0.0f, 0.05f, 0.0f);
-            }
+        if (CurrentHealth == 100)
+        {
+            Vida100.SetActive(true);
+            Vida80.SetActive(true);
+            Vida60.SetActive(true);
+            Vida40.SetActive(true);
+            Vida20.SetActive(true);
+        }
+        else if (CurrentHealth >= 80) {
+            Vida100.SetActive(false);
+            Vida80.SetActive(true);
+            Vida60.SetActive(true);
+            Vida40.SetActive(true);
+            Vida20.SetActive(true);
+
+            //if (Vida100.transform.localScale.y <= 0.0f)
+            //{
+            //    Vida100.SetActive(false);
+            //}
+            //else
+            //{
+            //    Vida100.transform.localScale = new Vector3(0.0f, 0.05f, 0.0f);
+            //}
         }
 
-        if (CurrentHealth == 60)
+        else if (CurrentHealth >= 60)
         {
-            if (Vida80.transform.localScale.y <= 0.0f)
-            {
-                Vida80.SetActive(false);
-            }
-            else
-            {
-                Vida80.transform.localScale = new Vector3(0.0f, 0.05f, 0.0f);
-            }
+            Vida100.SetActive(false);
+            Vida80.SetActive(false);
+            Vida60.SetActive(true);
+            Vida40.SetActive(true);
+            Vida20.SetActive(true);
+            //if (Vida80.transform.localScale.y <= 0.0f)
+            //{
+            //    Vida80.SetActive(false);
+            //}
+            //else
+            //{
+            //    Vida80.transform.localScale = new Vector3(0.0f, 0.05f, 0.0f);
+            //}
         }
 
-        if (CurrentHealth == 40)
+        else if (CurrentHealth >= 40)
         {
-            if (Vida60.transform.localScale.y <= 0.0f)
-            {
-                Vida60.SetActive(false);
-            }
-            else
-            {
-                Vida60.transform.localScale = new Vector3(0.0f, 0.05f, 0.0f);
-            }
+            Vida100.SetActive(false);
+            Vida80.SetActive(false);
+            Vida60.SetActive(false);
+            Vida40.SetActive(true);
+            Vida20.SetActive(true);
+            //if (Vida60.transform.localScale.y <= 0.0f)
+            //{
+            //    Vida60.SetActive(false);
+            //}
+            //else
+            //{
+            //    Vida60.transform.localScale = new Vector3(0.0f, 0.05f, 0.0f);
+            //}
         }
 
-        if (CurrentHealth == 20)
+        else if (CurrentHealth >= 20)
         {
-            if (Vida40.transform.localScale.y <= 0.0f)
-            {
-                Vida40.SetActive(false);
-                //Critical();
-                //CriticalLife.SetActive(true);
-            }
-            else
-            {
-                Vida40.transform.localScale = new Vector3(0.0f, 0.05f, 0.0f);
-            }
+            Vida100.SetActive(false);
+            Vida80.SetActive(false);
+            Vida60.SetActive(false);
+            Vida40.SetActive(false);
+            Vida20.SetActive(true);
+            //if (Vida40.transform.localScale.y <= 0.0f)
+            //{
+            //    Vida40.SetActive(false);
+            //    //Critical();
+            //    //CriticalLife.SetActive(true);
+            //}
+            //else
+            //{
+            //    Vida40.transform.localScale = new Vector3(0.0f, 0.05f, 0.0f);
+            //}
         }
 
-        if (CurrentHealth == 0)
+        else //if (CurrentHealth >= 0)
         {
-            if (Vida20.transform.localScale.y <= 0.0f)
-            {
-                Vida20.SetActive(false);
-                
-            }
-            else
-            {
-                Vida20.transform.localScale = new Vector3(0.0f, 0.05f, 0.0f);
-            }
+            Vida100.SetActive(false);
+            Vida80.SetActive(false);
+            Vida60.SetActive(false);
+            Vida40.SetActive(false);
+            Vida20.SetActive(false);
+            //if (Vida20.transform.localScale.y <= 0.0f)
+            //{
+            //    Vida20.SetActive(false);
+
+            //}
+            //else
+            //{
+            //    Vida20.transform.localScale = new Vector3(0.0f, 0.05f, 0.0f);
+            //}
         }
 
       
